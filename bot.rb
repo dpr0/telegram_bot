0ed573ef4d7ac0b62e28f78a6acadba171be8489
dvitvitskiy <dvitvitskiy@npoprogress.com>
2020-11-18 17:20:21 +0300

    32

--- bot.rb
+++ bot.rb
@@ -7,28 +7,38 @@ require_relative 'models/application_record'
 require_relative 'models/dictionary'
 Dir['models/*.rb'].each { |file| require_relative file }
 
-TBT = Telegram::Bot::Types
-BTN = lambda { |str| TBT::KeyboardButton.new(text: str) }
-
 Telegram::Bot::Client.run(ENV['BOT_TOKEN'], logger: Logger.new($stderr)) do |bot|
   bot.listen do |message|
-    Message.create(uid: message.from.id, username: message.from.username, text: message.text)
+    Msg.new(message).event
     bot.logger.info(message.text)
-    text = message.text.tr('/', '').to_i if message.text
-    num = text.to_i
+  end
+end
+
+class Msg
+
+  TBT = Telegram::Bot::Types
+  BTN = lambda { |str| TBT::KeyboardButton.new(text: str) }
+  PHOTO = lambda { |num| Faraday::UploadIO.new("images/players/#{Player.photo_nums.include?(num) ? num : 'anonim'}.jpg", 'image/jpeg') }
+
+  def initialize(message)
+    Message.create(uid: message.from.id, username: message.from.username, text: message.text)
+    @text = message.text.tr('/', '') if message.text
+    @num = @text.to_i
+    @message = message
+  end
+
+  def event
     if num != 0
       player = Player.find_by(id: num)
       if player
-        name = Player.photo_nums.include?(num) ? num : 'anonim'
-        bot.api.send_photo(chat_id: message.chat.id, photo: Faraday::UploadIO.new("images/players/#{name}.jpg", 'image/jpeg'))
-        bot.api.send_message(chat_id: message.chat.id, text: player.print_stat)
+        bot.api.send_photo(chat_id: message.chat.id, caption: player.print_stat, photo: PHOTO.(num))
       else
         bot.api.send_message(chat_id: message.chat.id, text: 'Нет такого игрока')
       end
     else
       case text
       when 'start'
-        buttons = [[BTN.call('break'), BTN.call('stop')]]
+        buttons = [[BTN.('break'), BTN.('stop')]]
         markup = TBT::ReplyKeyboardMarkup.new(keyboard: buttons, one_time_keyboard: true, resize_keyboard: true)
         bot.api.send_message(chat_id: message.chat.id, text: "Привет #{message.from.first_name}! Выберите действие:", reply_markup: markup)
       when 'stop'
@@ -38,5 +48,6 @@ Telegram::Bot::Client.run(ENV['BOT_TOKEN'], logger: Logger.new($stderr)) do |bot
         # bot.api.send_message(chat_id: message.chat.id, text: "#{message.from.first_name}, я не понимаю тебя 🤷‍")
       end
     end
+
   end
 end
