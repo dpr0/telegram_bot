5159eec69352d54683df966f5e1a59bd6b17d852
dvitvitskiy <dvitvitskiy@npoprogress.com>
2020-11-18 17:30:18 +0300

    fix

--- bot.rb
+++ bot.rb
@@ -13,32 +13,33 @@ class Msg
   BTN = lambda { |str| TBT::KeyboardButton.new(text: str) }
   PHOTO = lambda { |num| Faraday::UploadIO.new("images/players/#{Player.photo_nums.include?(num) ? num : 'anonim'}.jpg", 'image/jpeg') }
 
-  def initialize(message)
+  def initialize(bot, message)
     Message.create(uid: message.from.id, username: message.from.username, text: message.text)
     @text = message.text.tr('/', '') if message.text
     @num = @text.to_i
     @message = message
+    @bot = bot
   end
 
   def event
-    if num != 0
-      player = Player.find_by(id: num)
+    if @num != 0
+      player = Player.find_by(id: @num)
       if player
-        bot.api.send_photo(chat_id: message.chat.id, caption: player.print_stat, photo: PHOTO.(num))
+        @bot.api.send_photo(chat_id: @message.chat.id, caption: player.print_stat, photo: PHOTO.(@num))
       else
-        bot.api.send_message(chat_id: message.chat.id, text: 'Нет такого игрока')
+        @bot.api.send_message(chat_id: @message.chat.id, text: 'Нет такого игрока')
       end
     else
-      case text
+      case @text
       when 'start'
         buttons = [[BTN.('break'), BTN.('stop')]]
         markup = TBT::ReplyKeyboardMarkup.new(keyboard: buttons, one_time_keyboard: true, resize_keyboard: true)
-        bot.api.send_message(chat_id: message.chat.id, text: "Привет #{message.from.first_name}! Выберите действие:", reply_markup: markup)
+        @bot.api.send_message(chat_id: @message.chat.id, text: "Привет #{@message.from.first_name}! Выберите действие:", reply_markup: markup)
       when 'stop'
         kb = TBT::ReplyKeyboardRemove.new(remove_keyboard: true)
-        bot.api.send_message(chat_id: message.chat.id, text: 'stop', reply_markup: kb)
+        @bot.api.send_message(chat_id: @message.chat.id, text: 'stop', reply_markup: kb)
       else
-        # bot.api.send_message(chat_id: message.chat.id, text: "#{message.from.first_name}, я не понимаю тебя 🤷‍")
+        # bot.api.send_message(chat_id: @message.chat.id, text: "#{@message.from.first_name}, я не понимаю тебя 🤷‍")
       end
     end
 
@@ -47,7 +48,7 @@ end
 
 Telegram::Bot::Client.run(ENV['BOT_TOKEN'], logger: Logger.new($stderr)) do |bot|
   bot.listen do |message|
-    Msg.new(message).event
+    Msg.new(bot, message).event
     bot.logger.info(message.text)
   end
 end
